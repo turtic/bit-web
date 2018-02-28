e9c61034edc6c1987ced4ca6d530260f5b58106f
BIT Student <classroom@bgit.rs>
2018-02-28 18:22:35 +0100

    events

---
+++ JSinHTML/DOM-Exercises-Football/main.js
@@ -0,0 +1,18 @@
+
+// var myInput = function () {
+
+//     var text = document.querySelectorAll('input').value;
+
+//     var newEl = document.createElement('p');
+
+//     var newText = document.createTextNode(text);
+
+//     newEl.appendChild(newText);
+
+//     someList = document.getElementById('screen')
+
+//     someList.appendChild(newEl);
+
+// };
+
+
